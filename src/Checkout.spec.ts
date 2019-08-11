b1c250814ea2db34126a6ec0c6625c259f70b2d6
Callum McClean <callummccleandev@gmail.com>
2019-08-11 11:59:09 +0100

    Refactor

--- src/Checkout.spec.ts
+++ src/Checkout.spec.ts
@@ -1,27 +1,29 @@
 import Checkout from './Checkout';
 
+const price = (items: string): number => {
+  const checkout = new Checkout();
+  items.split('').forEach((item: string) => {
+    checkout.scan(item);
+  });
+  return checkout.getTotalPrice();
+};
+
 describe('Totals with no special prices', () => {
 
   it('returns 0 when no items scanned', () => {
-    const checkout = new Checkout();
-    const total = checkout.getTotalPrice();
+    const total = price('');
 
     expect(total).toBe(0);
   });
 
   it('returns 50 when A scanned', () => {
-    const checkout = new Checkout();
-    checkout.scan('A');
-    const total = checkout.getTotalPrice();
+    const total = price('A');
 
     expect(total).toBe(50);
   });
 
   it('returns 80 when AB scanned', () => {
-    const checkout = new Checkout();
-    checkout.scan('A');
-    checkout.scan('B');
-    const total = checkout.getTotalPrice();
+    const total = price('AB');
 
     expect(total).toBe(80);
   });
