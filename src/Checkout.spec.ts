67056f0614720e476f3ec682ec254eae66611f22
Callum McClean <callummccleandev@gmail.com>
2019-08-11 11:48:51 +0100

    Red Test - returns 50 when A scanned

--- src/Checkout.spec.ts
+++ src/Checkout.spec.ts
@@ -8,4 +8,12 @@ describe('Totals with no special prices', () => {
 
     expect(total).toBe(0);
   });
+
+  it('returns 50 when A scanned', () => {
+    const checkout = new Checkout();
+    checkout.scan('A');
+    const total = checkout.getTotalPrice();
+
+    expect(total).toBe(50);
+  });
 });
