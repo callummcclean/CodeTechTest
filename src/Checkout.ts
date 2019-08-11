a7abb38e9365d7743220d243d7e243e103026b5d
Callum McClean <callummccleandev@gmail.com>
2019-08-11 12:14:36 +0100

    Green test - AAA discounted

--- src/Checkout.ts
+++ src/Checkout.ts
@@ -4,17 +4,25 @@ import IPriceProvider from './IPriceProvider';
 class Checkout implements ICheckout {
   priceList: IPriceProvider;
   subTotal: number;
+  countOfA: number;
 
   constructor(priceList: IPriceProvider) {
     this.priceList = priceList;
     this.subTotal = 0;
+    this.countOfA = 0;
   }
 
   scan(item: string): void {
     this.subTotal += this.priceList.get(item);
+    if (item === 'A') {
+      this.countOfA += 1;
+    }
   }
 
   getTotalPrice(): number {
+    if (this.countOfA === 3) {
+      return this.subTotal - 20;
+    }
     return this.subTotal;
   }
 }
